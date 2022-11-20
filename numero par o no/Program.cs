462344ee5fe3c8b190900866040791a0dba34629
fernanjps <118408440+fernanjps@users.noreply.github.com>
2022-11-20 01:08:50 -0500

    Add files via upload

---
+++ numero par o no/Program.cs
@@ -0,0 +1,20 @@
+﻿/* nombre: fernanjps3A  */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //determinar si un numero es par 
+        Console.Write("introducir el primer numero: ");
+        double numero = Convert.ToDouble(Console.ReadLine());
+        if(numero % 2 == 0)
+        {
+            Console.Write("el numero" + numero + "es par.");
+        }
+        else
+        {
+            Console.Write("el numero " + numero + " es impar.");
+        }
+        Console.ReadLine();
+    }
+}
