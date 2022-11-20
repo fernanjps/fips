e4241b44888127d9d326b595e3bf3f0d1fee9da9
fernanjps <118408440+fernanjps@users.noreply.github.com>
2022-11-20 01:23:26 -0500

    Add files via upload

---
+++ que triangulo es/Program.cs
@@ -0,0 +1,28 @@
+﻿/* nombre: fernanjps3A       */
+class Program
+{
+    static void Main(string[] args)
+    {
+        double n1, n2, n3;
+        Console.WriteLine("primer lado:");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("segundo lado:");
+        n2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("tercer lado:");
+        n3 = Convert.ToDouble(Console.ReadLine());
+        if (n1 == n2 && n1==n3)
+            Console.WriteLine("es equilatero");
+        else    
+        {
+            if (n1== n2 ||n1 ==n3 || n3==n2)
+                Console.WriteLine("es isosceles");
+            else
+            {
+                if (n1 != n2 || n2!=n3 || n3 != n1)
+                    Console.WriteLine("es escaleno");
+            }    
+        }
+        Console.Read();
+
+    }
+}
